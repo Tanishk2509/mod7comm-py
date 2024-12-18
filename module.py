e287047a48a060eed6510528f61ada8ca894dcce
Tanishk Kumar <kumartanishk216@gmail.com>
2024-12-18 20:49:13 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,5 @@
+import pyjokes
+
+# print("hello world")
+joke = pyjokes.get_joke()
+print(joke)
---
+++ problem1.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("siya kyaaa haall hai")
+engine.runAndWait()
